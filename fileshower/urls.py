c9c2161ba4c7aa848d68b5f025efba00535a98a7
Dmitry Krasilov <dmitrykrasilov@C0005.soni.redsolution.ru>
2014-06-19 16:06:14 +0600

    fixed in CBV-style

--- fileshower/urls.py
+++ fileshower/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from views import UploadFormView
+
 urlpatterns = patterns('fileshower.views',
-    url('^$', 'upload_file', name = 'upload_file'),
+    url('^$', UploadFormView.as_view(), name='upload_file'),
 )
+
--- fileshower/views.py
+++ fileshower/views.py
@@ -1,30 +1,30 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import FormView
 
 from forms import DocumentForm
 
+class UploadFormView(FormView):
 
-def upload_file(request):
-    file_content = ''
-    file_exist = False
-    
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            file_content = request.FILES.values()[0].read()
+    template_name = 'mainpage.html'
+    form_class = DocumentForm
+    success_url = '/'
+        
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        file_exist = False
+        file_content = self.request.FILES.values()[0].read()
  
-            # Checking for MIME type.
-            if request.FILES.values()[0].content_type.find('text') != -1:
-                file_exist = True
-            
-    else:
-        form = DocumentForm()
-    
-    return render_to_response('mainpage.html',
+        # Checking for MIME type.
+        if self.request.FILES.values()[0].content_type.find('text') != -1:
+            file_exist = True
+        
+        return render_to_response('mainpage.html',
                                      {'form': form,
                                  'file_data': file_content,
                                 'file_exist': file_exist},
-                              context_instance=RequestContext(request)
+                              context_instance=RequestContext(self.request)
                               )
-
+  
