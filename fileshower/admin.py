908deba18e820660734cb40578543f69b43d544b
Dmitry Krasilov <dmitrykrasilov@C0005.soni.redsolution.ru>
2014-06-20 17:11:11 +0600

    filename extension filter for django admin added

--- fileshower/admin.py
+++ fileshower/admin.py
@@ -5,19 +5,33 @@ from django import forms
 from fileshower.models import Document
 
 class FileTypeListFilter(admin.SimpleListFilter):
-    title = _('Type of file')
+    title = _('File type')
 
     parameter_name = 'type'
     
-"""
     def lookups(self, request, model_admin):
         
         return (
             ('py', _('.py files')),
             ('txt', _('.txt files')),
-            ('other', _('other files'),
+            ('other', _('Other files')),
         )
-"""    
+        
+    def queryset(self, request, queryset):
+    
+        if self.value() == 'py':
+            tmp_id_queryset = [x.id for x in queryset if x.name.split(".")[-1] == 'py']
+            return queryset.filter(pk__in=tmp_id_queryset)
+            
+        if self.value() == 'txt':
+            tmp_id_queryset = [x.id for x in queryset if x.name.split(".")[-1] == 'txt']
+            return queryset.filter(pk__in=tmp_id_queryset)
+            
+        if self.value() == 'other':
+            special = ('py','txt')
+            tmp_id_queryset = [x.id for x in queryset if x.name.split(".")[-1] not in special]
+            return queryset.filter(pk__in=tmp_id_queryset)
+            
 
 class DocumentAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -38,7 +52,7 @@ class DocumentAdmin(admin.ModelAdmin):
     fields = ('content', 'name', 'lastviewed',)
     list_display = ('name', 'lastviewed',)
     readonly_fields = ('name', 'lastviewed',)
-    list_filter = ('name','lastviewed')
+    list_filter = ('name','lastviewed', FileTypeListFilter,)
     form = DocumentAdminForm
 
 
