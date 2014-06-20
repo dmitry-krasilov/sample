02dd88f630c0be22a8dc8f5f545b0f5a4a9b84ce
Dmitry Krasilov <dmitrykrasilov@C0005.soni.redsolution.ru>
2014-06-20 11:09:22 +0600

    render_to_response() fixed

--- fileshower/views.py
+++ fileshower/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import FormView
@@ -13,19 +12,21 @@ class UploadFormView(FormView):
     success_url = '/'
         
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
+    
         file_exist = False
         file_content =''
+        
         # Checking for MIME type.
         if 'text' in self.request.FILES.values()[0].content_type:
             file_content = self.request.FILES.values()[0].read()
             file_exist = True
         
-        return render_to_response('mainpage.html',
-                                     {'form': form,
-                                 'file_data': file_content,
-                                'file_exist': file_exist},
-                              context_instance=RequestContext(self.request)
-                              )
-  
+        return render_to_response(
+            'mainpage.html',
+            {
+             'form': form,
+             'file_data': file_content,
+             'file_exist': file_exist
+            },
+            context_instance=RequestContext(self.request)
+        )
